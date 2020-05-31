195d502dcbe0dd4eff6c5adcfab1a6113e7c02f5
Osebobo <oladimejiadeyemi@gmail.com>
2020-05-31 05:20:24 +0100

    stage 2 and 3

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +13,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/secretDB", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
