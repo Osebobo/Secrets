6537437d8bf7f48d49ff664895b06b1cf8d2259d
Osebobo <oladimejiadeyemi@gmail.com>
2020-05-31 10:30:42 +0100

    stage 4 salting and hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -26,6 +27,31 @@ app.get("/", function(req,res){
   res.render("home");
 });
 
+app.route("/register")
+
+.get(function(req,res){
+  res.render("register");
+})
+
+.post(function(req,res){
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        res.send(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+
+});
+
+
 app.route("/login")
 
 .get(function(req,res){
@@ -34,40 +60,24 @@ app.route("/login")
 
 .post(function(req,res){
     const username= req.body.username;
-    const password= md5(req.body.password);
+    const password= req.body.password;
 
     User.findOne({email: username}, function(err, foundUser){
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          }
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
+         
         }
       }
     });
 });
 
-app.route("/register")
-
-.get(function(req,res){
-  res.render("register");
-})
-
-.post(function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if (err) {
-      res.send(err);
-    } else {
-      res.render("secrets");
-    }
-  });
-});
 
 
 
