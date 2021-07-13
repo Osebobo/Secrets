70146f79efa85d12eabe3844a9d30dc5ae0a6841
oladimeji adeyemi <oladimejiadeyemi@gmail.com>
2021-07-13 06:05:51 +0100

    modified app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/secretDB", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
+mongoose.connect("mongodb://localhost:27017/secretsDB", {useNewUrlParser: true, useUnifiedTopology:true, useFindAndModify: false});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
